08c38c882f379be29adc56834d8f9b6c017acc5c
sonimus <soniaparrilla4a@gmail.com>
2014-10-08 18:55:29 +0200

    menu inicial

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.GetKey (KeyCode.Escape)) {
+						reiniciar ();
+	
+				}
+		}
+
+	public void salir(){
+		Application.Quit ();
+}
+	public void reiniciar(){
+		Application.LoadLevel ("menu");
+	}
+
+	public void iniciar(){
+		Application.LoadLevel ("Sonia");
+	}
+}
