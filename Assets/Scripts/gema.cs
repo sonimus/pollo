7f12f4d6879f633ccabc50041ea0cafc9d83ad00
sonimus <soniaparrilla4a@gmail.com>
2014-10-09 19:22:02 +0200

    enemigos
    añadir enemigos y hacer de de la gema pase al nivel 2 (sonia_01)

---
+++ Assets/Scripts/gema.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gema : MonoBehaviour {
+
+	public string nivel = "sonia";
+		void OnTriggerEnter2D(Collider2D other){
+		if(other.transform.tag == "Player")
+			Application.LoadLevel (nivel);
+	}
+}
